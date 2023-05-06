09eced07acdb6a6d68d7231740c1e4a40d080050
Iverkobs8 <kobepatalinghugiverson17@gmail.com>
2023-05-06 13:34:54 +0400

    Txt search issue solved

--- Transactions.cs
+++ Transactions.cs
@@ -136,10 +136,14 @@ namespace Finals
         private void txtsearch_TextChanged(object sender, EventArgs e)
         {
             con.Open();
-            SqlCommand com = new SqlCommand("SELECT o.Order_ID , p.Product_ID as 'Product ID', p.Image_apparel as Item," +
-                " p.Name, p.Size, c.Quantity, c.Price, o.Date_Purchased as 'Date Purchased' FROM Products p INNER JOIN Backup_Cart c ON" +
-                " p.Product_ID = c.Product_ID INNER JOIN Orders o ON o.Product_ID = p.Product_ID AND o.Username = c.Username" +
-                " WHERE Order_ID like'%"+txtsearch.Text+"'", con);
+            SqlCommand com = new SqlCommand("SELECT o.Order_ID, p.Product_ID as 'Product ID', p.Image_apparel as Item, " +
+                "p.Name, p.Size, c.Quantity, c.Price, o.Date_Purchased as 'Date Purchased' " +
+                "FROM Products p " +
+                "INNER JOIN Backup_Cart c ON p.Product_ID = c.Product_ID " +
+                "INNER JOIN Orders o ON o.Product_ID = p.Product_ID AND o.Username = c.Username " +
+                "WHERE o.Order_ID LIKE @searchText", con);
+
+            com.Parameters.AddWithValue("@searchText", "%" + txtsearch.Text + "%");
 
             SqlDataAdapter adap = new SqlDataAdapter(com);
             DataTable tab = new DataTable();
@@ -148,6 +152,7 @@ namespace Finals
             dataGridView1.DataSource = tab;
 
             con.Close();
+
         }
     }
 }
